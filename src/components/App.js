2b8a284be7fd28c1adee2578fb4f990988a32499
James Richardson <James.Richardson@invertedlight.com>
2018-04-18 16:17:06 -0500

    create NavbarVG Logos file. add TeamLogo file
    Setup the Navbar and add to App.js
    Add the default Route to show 404 page if needed.
    Add S

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route }	from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch }	from 'react-router-dom'
 import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
-    	<Router>
-	      <div >
-	        
-	        <Route path='/' exact component={Home} />
-	        <Route path='/Players' component={Players} />
-	        <Route path='/Teams'	component={Teams} />
-	      </div>
-      </Router>
+    	
+	    	<Router>
+		      <div >
+			      <Navbar />
+
+			      <Switch>
+			        <Route path='/' exact component={Home} />
+			        <Route path='/Players' component={Players} />
+			        <Route path='/Teams'	component={Teams} />
+			        <Route render={({ location }) => <div><h1 className='text-center'>Four Oh Four.</h1>
+			      												<h2 className='text-center'>'{location.pathname}' - Does not exist!</h2></div> } />
+            </Switch>
+		      </div>
+	      </Router>
     )
+
+
+
   }
 }
 
