c869a99d73e68c79a45ba14b91eeddd3b2d910c4
James Richardson <James.Richardson@invertedlight.com>
2018-04-23 18:42:51 -0500

    Fix Home page Teams display to be centered.
    Add the  className='home-grid' to the <Div  > around the teams.map code.

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,7 @@ export default class Home extends Component {
 				<h3 className='header text-center'>
 					Select a teamName
 				</h3>
-				<div>
+				<div className='home-grid'>
 					{teamNames.map((id) => (
 						<Link key={id} to={`/${id}`}>
 							<TeamLogo id={id} width='125px' />
