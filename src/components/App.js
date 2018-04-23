9a812e629221f6b31194b18b3f6f6470795a8ebf
James Richardson <James.Richardson@invertedlight.com>
2018-04-23 14:03:12 -0500

    Build out TeamPage.
    Add Team Stats.
    Add Reference to articles and links.
    Add redirect for a non team back to the pick teams page.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
 import Navbar from './Navbar'
+import TeamPage from './TeamPage'
 
 class App extends Component {
   render() {
@@ -15,8 +16,9 @@ class App extends Component {
 
 			      <Switch>
 			        <Route path='/' exact component={Home} />
-			        <Route path='/Players' component={Players} />
-			        <Route path='/Teams'	component={Teams} />
+			        <Route path='/players' component={Players} />
+			        <Route path='/teams'	component={Teams} />
+			        <Route path='/:teamId' exact component={TeamPage} />
 			        <Route render={({ location }) => <div><h1 className='text-center'>Four Oh Four.</h1>
 			      												<h2 className='text-center'>'{location.pathname}' - Does not exist!</h2></div> } />
             </Switch>
